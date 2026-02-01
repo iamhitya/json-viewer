8a59e7888bc8d22c711f5a41dce92cee19f6f484
Hiteshkumar Vaghasiya <hiteshvaghasiya1990@gmail.com>
2026-02-01 11:01:17 +0530

    feat: add Open Graph image and update rendering method in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+	const root = createRoot(rootElement);
+	root.render(<App />);
+}
